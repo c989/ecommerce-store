1e907b2c35ec2e3362659974df76ed1af5534097
Jonathan Clarke <Jclarke89@outlook.com>
2019-07-31 22:01:41 +0100

    Collection Item class and styling

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import CollectionItem from './../../components/collection-item/collection-item.component'
 import './preview-collection.style.scss';
 
 const PreviewCollection = ({ title, items }) => (
@@ -9,8 +9,8 @@ const PreviewCollection = ({ title, items }) => (
         <div className='preview'>
         {
             items.filter((item, idx) => idx < 4)
-            .map(item => (
-                <div key={item.id}>{item.name}</div>
+            .map(({ id, ...otherItemProps}) => (
+                <CollectionItem key={id} { ...otherItemProps } />
             ))
         }
 
