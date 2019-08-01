019d059b8f87a1c97b234c5135f350d3b5b06fea
Jonathan Clarke <Jclarke89@outlook.com>
2019-08-01 23:05:39 +0100

    sign in styling added including transition. Added Form Submit functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import FormInput from '../form-input/form-input.component';
 import './sign-in.style.scss';
 
 class SignIn extends React.Component{
@@ -30,23 +30,23 @@ class SignIn extends React.Component{
                 <span> Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                     name="email" 
                     type="email" 
                     value={this.state.email} 
-                    onChange={this.handleChange}
+                    handleChange={this.handleChange}
+                    label="email"
                     required 
                     />
-                    <label>Email</label>
 
-                    <input 
+                    <FormInput 
                     name="password" 
                     type="password"
                     value={this.state.password} 
-                    onChange={this.handleChange} 
+                    handleChange={this.handleChange} 
+                    label="label"
                     required 
                     />
-                    <label>Password</label>
                     <input type="submit" value="Submit" />
                 </form>
             
